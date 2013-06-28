ae1a5ac07aef009e34fcd7bb90f7daf8b50cb388
Sven Recknagel <svenrecknagel@googlemail.com>
2013-06-28 11:45:22 +0200

    changed build script to output a normal zip

--- package.sh
+++ package.sh
@@ -1,2 +1,6 @@
 sass --style compressed PrimePlayer/css/player.scss PrimePlayer/css/player.css
-crxmake --pack-extension=PrimePlayer --pack-extension-key=PrimePlayer.pem --ignore-file="\.(scss|map)$"
+#crxmake --pack-extension=PrimePlayer --pack-extension-key=PrimePlayer.pem --ignore-file="\.(scss|map)$"
+rm PrimePlayer.zip
+cd PrimePlayer
+7za a -xr@../exclude.lst -tzip ../PrimePlayer.zip *
+cd ..
