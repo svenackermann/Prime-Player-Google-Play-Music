cbc10f227808a17dc260cab57ce3f21becc60d8a
Sven Recknagel <svenrecknagel@googlemail.com>
2013-08-14 13:45:29 +0200

    improved build script

--- package.sh
+++
@@ -1,6 +0,0 @@
-sass --style compressed PrimePlayer/css/player.scss PrimePlayer/css/player.css
-#crxmake --pack-extension=PrimePlayer --pack-extension-key=PrimePlayer.pem --ignore-file="\.(scss|map)$"
-rm PrimePlayer.zip
-cd PrimePlayer
-7za a -xr@../exclude.lst -tzip ../PrimePlayer.zip *
-cd ..
