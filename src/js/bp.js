954cc253fcd132f515f3af059af7c90370a721f0
Sven Ackermann <svenrecknagel@gmail.com>
2015-04-24 08:02:15 +0200

    #115 removed app icon mask, updated rating image

--- src/js/bp.js
+++ src/js/bp.js
@@ -797,8 +797,7 @@ function fixForUri(string) {
 
   function createNotification(id, options, cb) {
     options.type = options.type || "basic";
-    options.iconUrl = options.iconUrl || "img/icon-48x48.png";
-    options.appIconMaskUrl = "img/icon-alpha.png";
+    options.iconUrl = options.iconUrl || "img/icon-128x128.png";
     if (localSettings.notificationsEnabled) chromeNotifications.create(id, options, function(nid) {
       notifications[nid] = { click: [], btnClick: [], close: [] };
       cb(nid);
